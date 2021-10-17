aa1497a0b69b72f03d0dc4f515505834b065c2d0
drull1000 <pcaetanop@protonmail.com>
2021-10-17 17:01:24 -0300

    Routes and search

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,14 +1,17 @@
 import { useState, useEffect, React } from "react";
 import StoreItem from "../StoreItem/StoreItem";
 import styles from "./ProductList.module.css";
+import { Link } from "react-router-dom";
 
-function ProductList() {
+function ProductList({ SearchQuery }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(process.env.REACT_APP_SERVER); //queries can be added here
+        const response = await fetch(
+          process.env.REACT_APP_SERVER + SearchQuery
+        );
         const data = await response.json();
         setProducts(data.products);
       } catch (e) {
@@ -16,23 +19,28 @@ function ProductList() {
       }
     };
     fetchData();
-  }, []);
+  }, [SearchQuery]);
 
   return (
-    <div className={styles.productList}>
-      {products.map((product) => (
-        <StoreItem
-          image={
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/220px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG"
-          }
-          id={product.id}
-          name={product.name}
-          price={product.price}
-          company={product.company}
-          rating={product.rating}
-        />
-      ))}
-    </div>
+    <>
+      <h1>Our products:</h1>
+      <div className={styles.productList}>
+        {products.map((product) => (
+          <Link to={"/Product"}>
+            <StoreItem
+              image={
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/220px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG"
+              }
+              id={product._id}
+              name={product.name}
+              price={product.price}
+              company={product.company}
+              rating={product.rating}
+            />
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
 
