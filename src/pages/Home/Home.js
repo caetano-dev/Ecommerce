c0a37bf95fec118137494ec478df640bcc81e680
drull1000 <pcaetanop@protonmail.com>
2021-10-19 15:29:23 -0300

    Moving components and functions

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import SearchBar from "../../components/SearchBar/SearchBar"
+import { useState, useEffect, React } from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import ProductList from "../../components/ProductList/ProductList";
+import { useLocation } from "react-router-dom";
 
-function Home() {
-    return (
-        <div>
-           <SearchBar/> 
-        </div>
-    )
-}
+const Home = () => {
+  const [productList, setProductList] = useState();
+  const searchQuery = useLocation().search;
 
-export default Home
+  const fetchData = async () => {
+    return await fetch("http://localhost:8080/api/v1/products" + searchQuery)
+      .then((response) => response.json())
+      .then((data) => {
+        setProductList(data.products);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <SearchBar />
+      <ProductList productList={productList} />
+    </div>
+  );
+};
+
+export default Home;
