913794a91f36ce6b8eff3ad5c1f2bfc6780a84bf
drull1000 <pcaetanop@protonmail.com>
2021-10-19 14:02:01 -0300

    Product page

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Products() {
+  const RouteParams = useLocation().search;
+  const name = new URLSearchParams(RouteParams).get("name");
+
   return (
     <div>
       <h1>Products</h1>
+      <img src="" alt="product image" />
+      <h1>{name}</h1>
+      <h2>product name</h2>
+      <p>Product price</p>
+      <p>Product company</p>
+      <p>Product rating</p>
     </div>
   );
 }
