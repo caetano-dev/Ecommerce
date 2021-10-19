40284d721fdf674ead153820578658806dd9147d
drull1000 <pcaetanop@protonmail.com>
2021-10-19 15:29:10 -0300

    Adding a form

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,35 +1,14 @@
 import { useState, useEffect, createRef, React } from "react";
 import styles from "./SearchBar.module.css";
-import ProductList from "../ProductList/ProductList";
 import { Route, BrowserRouter } from "react-router-dom";
-import Products from "../../pages/Products/Products";
 
-function SearchBar() {
-  const textInput = createRef();
-
-  const [searchQuery, setSearchQuery] = useState("");
-
-  function handleInput() {
-    setSearchQuery("?name=" + textInput.current.value);
-  }
-  console.log(searchQuery);
+const SearchBar = () => {
   return (
-    <>
-      <div className={styles.searchBar}>
-        <input type="text" ref={textInput} placeholder="Search for item." />
-        <button onClick={handleInput}>Search</button>
-      </div>
-
-      <BrowserRouter>
-        <Route exact path="/product" component={Products} />
-        <Route
-          exact
-          path="/"
-          component={() => <ProductList SearchQuery={searchQuery} />}
-        />
-      </BrowserRouter>
-    </>
+    <form action="/" method="get" className={styles.searchBar}>
+      <input type="text" placeholder="Search for items." name="name" />
+      <button type="submit">Search</button>
+    </form>
   );
-}
+};
 
 export default SearchBar;
