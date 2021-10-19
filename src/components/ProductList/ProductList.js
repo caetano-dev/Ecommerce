75384ddbfa65322edfa72f55d6895f030f5cd3e9
drull1000 <pcaetanop@protonmail.com>
2021-10-19 15:28:39 -0300

    Removing fetch function

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -3,45 +3,31 @@ import StoreItem from "../StoreItem/StoreItem";
 import styles from "./ProductList.module.css";
 import { Link } from "react-router-dom";
 
-function ProductList({ SearchQuery }) {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          process.env.REACT_APP_SERVER + SearchQuery
-        );
-        const data = await response.json();
-        setProducts(data.products);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    fetchData();
-  }, [SearchQuery]);
-
+const ProductList = ({ productList = [] }) => {
   return (
     <>
       <h1>Our products:</h1>
       <div className={styles.productList}>
-        {products.map((product) => (
-          <Link to={"/Product"}>
-            <StoreItem
-              image={
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/220px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG"
-              }
-              id={product._id}
-              name={product.name}
-              price={product.price}
-              company={product.company}
-              rating={product.rating}
-            />
-          </Link>
-        ))}
+        {productList.map((product) => {
+          if (product) {
+            return (
+              <StoreItem
+                image={
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/220px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG"
+                }
+                id={product._id}
+                name={product.name}
+                price={product.price}
+                company={product.company}
+                rating={product.rating}
+              />
+            );
+          }
+          return null;
+        })}
       </div>
     </>
   );
-}
+};
 
 export default ProductList;
