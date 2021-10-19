4624f705198c7232228e79f319db7088c4ddf111
drull1000 <pcaetanop@protonmail.com>
2021-10-19 14:02:14 -0300

    Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
 import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import { Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <SearchBar />
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from "../../components/SearchBar/SearchBar"
+
+function Home() {
+    return (
+        <div>
+           <SearchBar/> 
+        </div>
+    )
+}
+
+export default Home
