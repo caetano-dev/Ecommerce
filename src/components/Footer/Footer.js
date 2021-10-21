94b8f5b72585eb7d6c0daf8ce90dec49422b0a09
drull1000 <pcaetanop@protonmail.com>
2021-10-21 20:20:52 -0300

    Creating footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.Footer}>
+      <p>footer</p>
+    </footer>
+  );
+};
+export default Footer;
